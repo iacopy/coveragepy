882792fc67045d82088d92ec1c35eb8b5ac319a2
Iacopo Marmorini <iacopo@develer.com>
2016-06-20 17:05:42 +0200

    Add examples directory with a working example to experiment with coverage during development

---
+++ examples/README.rst
@@ -0,0 +1,45 @@
+Examples
+========
+
+This directory contains (mainly) some examples of python code and aims to be used for manual testing during coverage development.
+
+Also contains some useful bash script as example for the usage.
+
+
+* Setup
+
+.. code:: bash
+
+    # Setup virtualenv
+    $ [sudo] pip install virtualenv
+    $ cd go/to/coverage/root/directory
+    $ virtualenv -p python3.5 ENV
+
+    # Activate virtualenv
+    $ source ENV/bin/activate
+
+    # Build coverage
+    $ python setup.py install
+
+
+* Workflow
+
+.. code:: bash
+
+    # Update the code...
+    # Update the build:
+    $ python setup.py install
+
+    # Run (with pytracer) and see coverage results
+    $ coverage erase
+    $ coverage run --timid test_zero.py  # for example
+    $ coverage html
+    $ open htmlcov/index.html
+
+
+* Teardown
+
+.. code:: bash
+
+    # Deactivate the virtualenv
+    $ deactivate
---
+++ examples/module.py
@@ -0,0 +1,71 @@
+# module code
+
+
+def case(a):
+    if a:
+        d = a
+
+    b = 1 + d  # crash if a == 0 because d is not defined
+
+    return b
+
+
+for _ in range(5):
+    pass
+
+
+for _ in range(10):
+    pass
+
+
+for _ in range(20):
+    pass
+
+
+for _ in range(50):
+    pass
+
+
+for _ in range(100):
+    pass
+
+
+for _ in range(200):
+    pass
+
+
+for _ in range(500):
+    pass
+
+
+for _ in range(1000):
+    pass
+
+
+for _ in range(2000):
+    pass
+
+
+for _ in range(5000):
+    pass
+
+
+
+count = 0
+for i1 in range(2):
+    for i2 in range(2):
+        for i3 in range(2):
+            for i4 in range(2):
+                for i5 in range(2):
+                    for i6 in range(2):
+                        for i7 in range(2):
+                            for i8 in range(2):
+                                for i9 in range(2):
+                                    for i10 in range(2):
+                                        for i11 in range(2):
+                                            for i12 in range(2):
+                                                for i13 in range(2):
+                                                    for i14 in range(2):
+                                                        for i15 in range(2):
+                                                            for i16 in range(2):
+                                                                count += 1
